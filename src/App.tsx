7a98525b6e25318baf8c4728ef5338aebeb37802
psycho-baller <Ramim66809@gmail.com>
2022-08-15 00:03:41 +0400

    im confident

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import { motion } from 'framer-motion';
 import './App.css';
 
 function App() {
+    const ferrisOfTechs = [
+      // "next-js.png",
+      "react-js.svg",
+      "typescript.svg",
+      "python.svg",
+      "java.svg",
+      "sqlite.svg",
+    //   "jupyter.svg",
+      "node-js.svg",
+      "firebase.svg",
+      "mongodb.svg",
+    //   "tailwindcss.svg",
+    //   "chakra-ui.jpg",
+    //   "framer-motion.webp",
+    //   "html.svg",
+    //   "scss.svg",
+    ];
   return (
     <div className="App">
-      <header className="App-header">
+      {ferrisOfTechs.map((tech, index: number) => {
+        return (
+          <motion.div
+            className="ferris-wheel-techs"
+            key={index + 1}
+            initial="initial"
+            animate={["animate", "initialHide"]}
+            exit="exit"
+            variants={{
+              animate: {
+                rotate: -360,
+                transition: {
+                  duration: ferrisOfTechs.length,
+                  repeat: Infinity,
+                  delay: index + 1,
+                  ease: "linear",
+                },
+              },
+              initial: {
+                opacity: 0,
+              },
+              initialHide: {
+                opacity: 1,
+                transition: {
+                  delay: index + 1,
+                },
+              },
+            }}
+          >
+            <div className="right-center">
+              <img
+                className='tech-icon'
+                src={require(`./tech-icons/${tech}`)}
+                alt={tech}
+              />
+            </div>
+          </motion.div>
+        );
+      }, [])}
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +73,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
